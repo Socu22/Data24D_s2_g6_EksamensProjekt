61d7f634801eeacc64abecfabbd180d7289c57fd
LucasPK <149070654+Socu22@users.noreply.github.com>
2025-05-02 12:22:10 +0200

    Initial commit

---
+++ src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/Data24DS2G6EksamensProjektApplication.java
@@ -0,0 +1,13 @@
+package com.g6.data24d_s2_g6_eksamensprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data24DS2G6EksamensProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Data24DS2G6EksamensProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g6/data24d_s2_g6_eksamensprojekt/Data24DS2G6EksamensProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g6.data24d_s2_g6_eksamensprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data24DS2G6EksamensProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
