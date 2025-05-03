9aa9efe7f614998006b8e860a27607395a38e3be
LucasPK <149070654+Socu22@users.noreply.github.com>
2025-05-03 13:08:12 +0200

    setups v2.11

--- src/main/resources/setup.sql
+++ src/main/resources/setup.sql
@@ -8,4 +8,4 @@ create table lager(lager_Id int primary key auto_increment not null ,navn varcha
 create table bil(vognNummer varchar(7) primary key not null , stelNummer varchar(17) not null,bilType_Id int not null, lager_Id int not null ,status varchar(20) not null, unique (stelNummer), foreign key (bilType_Id) references bilType(bilType_Id), foreign key (lager_Id) references lager(lager_Id));
 create table kunde(kunde_Id int primary key auto_increment not null , navn varchar(50) not null );
 create table lejeAftaler(aftale_Id int primary key auto_increment not null , kunde_Id int not null , vognNummer varchar(7) not null , foreign key (kunde_Id) references kunde(kunde_Id), foreign key (vognNummer) references bil(vognNummer));
-create table notationer(notationer_id int primary key not null, aftale_Id int not null , beskrivelse varchar(255) not null, pris double not null , foreign key (aftale_Id) references lejeAftaler(aftale_Id));
+create table notationer(notationer_id int primary key auto_increment not null, aftale_Id int not null , beskrivelse varchar(255) not null, pris double not null , foreign key (aftale_Id) references lejeAftaler(aftale_Id));
