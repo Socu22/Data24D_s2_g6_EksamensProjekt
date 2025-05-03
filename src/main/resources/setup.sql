abe2ff6e697083a1e59b21cdebc207af28661330
LucasPK <149070654+Socu22@users.noreply.github.com>
2025-05-03 13:06:54 +0200

    setups v2.1

--- src/main/resources/setup.sql
+++ src/main/resources/setup.sql
@@ -2,10 +2,10 @@ DROP DATABASE IF EXISTS CarDatabase;
 CREATE DATABASE CarDatabase;
 USE CarDatabase;
 
-create table medArbejdere(medArbejder_Id int primary key , navn varchar(50), adgangskode varchar(50), stilling varchar(50));
-create table bilType(bilType_Id int primary key,mærke varchar(50), model varchar(50), udstyrsniveau varchar(50), stålPris double, afgift double, udledning_Co2 double);
-create table lager(lager_Id int primary key,navn varchar(50),adresse varchar(50));
-create table bil(vognNummer varchar(7) primary key, stelNummer varchar(17),bilType_Id int, lager_Id int,status varchar(20), unique (stelNummer), foreign key (bilType_Id) references bilType(bilType_Id), foreign key (lager_Id) references lager(lager_Id));
-create table kunde(kunde_Id int primary key, navn varchar(50));
-create table lejeAftaler(aftale_Id int primary key, kunde_Id int, vognNummer varchar(7), foreign key (kunde_Id) references kunde(kunde_Id), foreign key (vognNummer) references bil(vognNummer));
-create table notationer(notationer int primary key, aftale_Id int, beskrivelse varchar(255), pris double, foreign key (aftale_Id) references lejeAftaler(aftale_Id))
+create table medArbejdere(medArbejder_Id int primary key auto_increment not null , navn varchar(50) not null, adgangskode varchar(50) not null , stilling varchar(50) not null );
+create table bilType(bilType_Id int primary key auto_increment not null,mærke varchar(50) not null , model varchar(50) not null, udstyrsniveau varchar(50) not null, stålPris double not null , afgift double not null , udledning_Co2 double not null );
+create table lager(lager_Id int primary key auto_increment not null ,navn varchar(50) not null ,adresse varchar(50) not null );
+create table bil(vognNummer varchar(7) primary key not null , stelNummer varchar(17) not null,bilType_Id int not null, lager_Id int not null ,status varchar(20) not null, unique (stelNummer), foreign key (bilType_Id) references bilType(bilType_Id), foreign key (lager_Id) references lager(lager_Id));
+create table kunde(kunde_Id int primary key auto_increment not null , navn varchar(50) not null );
+create table lejeAftaler(aftale_Id int primary key auto_increment not null , kunde_Id int not null , vognNummer varchar(7) not null , foreign key (kunde_Id) references kunde(kunde_Id), foreign key (vognNummer) references bil(vognNummer));
+create table notationer(notationer_id int primary key not null, aftale_Id int not null , beskrivelse varchar(255) not null, pris double not null , foreign key (aftale_Id) references lejeAftaler(aftale_Id));
