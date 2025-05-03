b90815f9aceb2591c8bfff5f13cd9e36e5fba0ec
LucasPK <149070654+Socu22@users.noreply.github.com>
2025-05-03 12:52:14 +0200

    setup.sql v2

--- src/main/resources/setup.sql
+++ src/main/resources/setup.sql
@@ -3,7 +3,6 @@ CREATE DATABASE CarDatabase;
 USE CarDatabase;
 
 create table medArbejdere(medArbejder_Id int primary key , navn varchar(50), adgangskode varchar(50), stilling varchar(50));
-
 create table bilType(bilType_Id int primary key,mærke varchar(50), model varchar(50), udstyrsniveau varchar(50), stålPris double, afgift double, udledning_Co2 double);
 create table lager(lager_Id int primary key,navn varchar(50),adresse varchar(50));
 create table bil(vognNummer varchar(7) primary key, stelNummer varchar(17),bilType_Id int, lager_Id int,status varchar(20), unique (stelNummer), foreign key (bilType_Id) references bilType(bilType_Id), foreign key (lager_Id) references lager(lager_Id));
