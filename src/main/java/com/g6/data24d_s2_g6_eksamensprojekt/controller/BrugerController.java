69c637cfad19d105a356412315feb0de16abd9cc
alex <albo0003@kea.stud.dk>
2025-05-05 11:46:46 +0200

    opdatering til BrugerController

--- src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/controller/BrugerController.java
+++ src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/controller/BrugerController.java
@@ -1,5 +1,6 @@
 package com.g6.data24d_s2_g6_eksamensprojekt.controller;
 
+import com.g6.data24d_s2_g6_eksamensprojekt.model.Bruger;
 import com.g6.data24d_s2_g6_eksamensprojekt.repository.BrugerRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -25,27 +26,38 @@ public class BrugerController {
         return "index";
     }
     @GetMapping("/Logind")
-    public String logindSide() {
+    public String logindSide(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession(false);
+        if(session != null)
+            return "redirect:/";
+
+        String brugerIkkeFundet = request.getParameter("d");
+        model.addAttribute("brugerIkkeFundet", brugerIkkeFundet);
+
         return "logind";
     }
 
     @GetMapping("/OmdirigerLogind")
-    public String omdirigerLogindSide(HttpServletRequest request) {
+    public String omdirigerLogindSide(HttpServletRequest request, Model model) {
         String brugernavn = request.getParameter("brugernavn");
         String adgangskode = request.getParameter("adgangskode");
         HttpSession session = request.getSession(false);
         if(brugernavn != null && adgangskode != null){
-            //tjek om brugeren er i databasen
-            sætSession(request);
-            return "redirect:/";
+            Bruger bruger = brugerRepository.faaBruger(brugernavn, adgangskode);
+            if(bruger != null){
+                saetSession(request, bruger);
+                return "redirect:/";
+            }
+           // model.addAttribute("brugerIkkeFundet", true);
+            return "redirect:/Logind?d=true";
         }
 
         return "redirect:/Logind";
     }
-    private void sætSession(HttpServletRequest request){
-       // lav en bruger
+    private void saetSession(HttpServletRequest request, Bruger bruger){
+
         HttpSession session = request.getSession();
         session.setMaxInactiveInterval(1200);
-        //session.setAttribute("aktivBruger", bruger);
+        session.setAttribute("aktivBruger", bruger);
     }
 }
