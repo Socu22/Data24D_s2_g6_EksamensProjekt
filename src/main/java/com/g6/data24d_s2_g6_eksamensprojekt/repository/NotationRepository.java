6e584bb127d8d37a4eef5aa71c684c9688c210e3
Flamingotough <simongotfred@hotmail.com>
2025-05-06 12:58:16 +0200

    Oprettet Notations-Repository

---
+++ src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/repository/NotationRepository.java
@@ -0,0 +1,67 @@
+package com.g6.data24d_s2_g6_eksamensprojekt.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.*;
+
+import com.g6.data24d_s2_g6_eksamensprojekt.model.Bil;
+import com.g6.data24d_s2_g6_eksamensprojekt.model.Notation;
+import com.g6.data24d_s2_g6_eksamensprojekt.model.LejeAftale;
+
+@Repository
+public class NotationRepository
+{
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void addNotation(int vognNr, int aftaleId, String beskrivelse, double pris)
+    {
+        String sql = "INSERT INTO notationer (vognNummer, aftale_Id, beskrivelse, pris) VALUES (?, ?, ?, ?);";
+        jdbcTemplate.update(sql, vognNr, aftaleId, beskrivelse, pris);
+    }
+
+    public void updateNotation(Notation notation)
+    {
+        String sql = "UPDATE notationer SET vognNummer = ?, aftale_Id = ?, beskrivelse = ?, pris = ? WHERE notationer_Id = ?;";
+        jdbcTemplate.update(sql,
+                            notation.getVognNr(),
+                            notation.getAftaleId(),
+                            notation.getBeskrivelse(),
+                            notation.getPris(),
+                            notation.getId());
+    }
+
+    public List<Notation> getNotationer(LejeAftale aftale)
+    {
+        String sql = "SELECT * FROM notationer WHERE aftale_Id = ?;";
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql, aftale.getAftale_Id());
+
+        return buildNotationer(list);
+    }
+
+    public List<Notation> getNotationer(Bil bil)
+    {
+        String sql = "SELECT * FROM notationer WHERE vognNummer = ?;";
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql, bil.getVognNr());
+
+        return buildNotationer(list);
+    }
+
+    private List<Notation> buildNotationer(List<Map<String, Object>> dataList)
+    {
+        List<Notation> list = new ArrayList<>();
+
+        for (Map<String, Object> row:dataList)
+        {
+            list.add(new Notation((Integer) row.get("notationer_id"),
+                                  (Integer) row.get("vognNummer"),
+                                  (Integer) row.get("aftale_Id"),
+                                  (String)  row.get("beskrivelse"),
+                                  (Double)  row.get("pris")));
+        }
+
+        return list;
+    }
+}
