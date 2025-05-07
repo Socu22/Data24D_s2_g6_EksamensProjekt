df36d3bc33f21cdaebb665c383bf700a5a1532d6
Flamingotough <simongotfred@hotmail.com>
2025-05-07 11:15:39 +0200

    Opdateret BilType
    - med setters

--- src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/model/BilType.java
+++ src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/model/BilType.java
@@ -3,8 +3,10 @@ package com.g6.data24d_s2_g6_eksamensprojekt.model;
 public class BilType
 {
     private final int    bilTypeId;
-    private final String maerke, model, udstyr;
-    private final double staalPris, afgift, udledningCo2;
+    private String maerke, model, udstyr;
+    private double staalPris, afgift, udledningCo2;
+
+    public BilType(int bilTypeId){this.bilTypeId = bilTypeId;}
 
     public BilType(int bilTypeId, String maerke, String model, String udstyr, double staalPris, double afgift,
                    double udledningCo2)
@@ -22,34 +24,53 @@ public class BilType
     {
         return bilTypeId;
     }
-
     public String getMaerke()
     {
         return maerke;
     }
-
     public String getModel()
     {
         return model;
     }
-
     public String getUdstyr()
     {
         return udstyr;
     }
-
     public double getStaalPris()
     {
         return staalPris;
     }
-
     public double getAfgift()
     {
         return afgift;
     }
-
     public double getUdledningCo2()
     {
         return udledningCo2;
+    }
+
+    public void setMaerke(String maerke)
+    {
+        this.maerke = maerke;
+    }
+    public void setModel(String model)
+    {
+        this.model = model;
+    }
+    public void setUdstyr(String udstyr)
+    {
+        this.udstyr = udstyr;
+    }
+    public void setStaalPris(double staalPris)
+    {
+        this.staalPris = staalPris;
+    }
+    public void setAfgift(double afgift)
+    {
+        this.afgift = afgift;
+    }
+    public void setUdledningCo2(double udledningCo2)
+    {
+        this.udledningCo2 = udledningCo2;
     }
 }
