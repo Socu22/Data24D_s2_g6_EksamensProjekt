8aec3ed17e78213adeaf1e526003382e1d25fc60
Flamingotough <simongotfred@hotmail.com>
2025-05-07 10:16:40 +0200

    fix rudementært Bilrepo

--- src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/repository/BilRepository.java
+++ src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/repository/BilRepository.java
@@ -1,14 +1,21 @@
 package com.g6.data24d_s2_g6_eksamensprojekt.repository;
 
 import com.g6.data24d_s2_g6_eksamensprojekt.model.Bil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class BilRepository
 {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     public List<Bil> getBiler()
     {
-        return new ArrayList<Bil>();
+        return new ArrayList<>();
     }
 }
