e33d120e2db9b69ecebda8eb90ca19dbcd4a5ae2
Flamingotough <simongotfred@hotmail.com>
2025-05-07 17:06:56 +0200

    Opdateret Bil
    - defineret 'toString' til at returnere konventionel skrivelse af vognNummer

--- src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/model/Bil.java
+++ src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/model/Bil.java
@@ -1,11 +1,15 @@
 package com.g6.data24d_s2_g6_eksamensprojekt.model;
 
+import java.util.List;
+
 public class Bil
 {
     private final String vognNummer, stelNummer;
     private final BilType type;
     private int lager_Id;
     private String status;
+    private List<Notation> notationer;
+    private List<LejeAftale> lejeAftaler;
 
 
     public Bil(String vognNummer, String stelNummer, BilType type, int lager_Id, String status) {
@@ -43,8 +47,16 @@ public class Bil
     public void setStatus(String status) {
         this.status = status;
     }
+
     public int getBilType_Id(){
 
         return type.getBilTypeId();
+    }
+
+    public String toString()
+    {
+        StringBuilder s = new StringBuilder(vognNummer);
+        s.insert(4,' ').insert(2,' ');
+        return s.toString();
     }
 }
