ac03b59a115f3d9ebfbb8166775ee3cce2f7f6fc
Flamingotough <simongotfred@hotmail.com>
2025-05-07 10:11:01 +0200

    Rudementært BilRepo

---
+++ src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/repository/BilRepository.java
@@ -0,0 +1,14 @@
+package com.g6.data24d_s2_g6_eksamensprojekt.repository;
+
+import com.g6.data24d_s2_g6_eksamensprojekt.model.Bil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BilRepository
+{
+    public List<Bil> getBiler()
+    {
+        return new ArrayList<Bil>();
+    }
+}
