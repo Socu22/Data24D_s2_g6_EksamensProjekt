0d8f86c80df5ce3003ab47d864518fa801137b3b
LucasPK <149070654+Socu22@users.noreply.github.com>
2025-05-20 12:51:25 +0200

    demoSetup.sql Færdig.

--- src/main/resources/demoSetup.sql
+++ src/main/resources/demoSetup.sql
@@ -33,7 +33,7 @@ CREATE TABLE bil(
                     bilType_Id INT NOT NULL,
                     lager_Id INT NOT NULL,
                     status VARCHAR(20) NOT NULL,
-                    kørteKm    DOUBLE  DEFAULT 0,
+                    kørteKm DOUBLE DEFAULT 0,
                     UNIQUE (stelNummer),
                     FOREIGN KEY (bilType_Id) REFERENCES bilType(bilType_Id),
                     FOREIGN KEY (lager_Id) REFERENCES lager(lager_Id)
@@ -41,7 +41,7 @@ CREATE TABLE bil(
 
 CREATE TABLE lejeAftaler(
                             aftale_Id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                            kunde_Navn varchar(50) NOT NULL,
+                            kunde_Navn VARCHAR(50) NOT NULL,
                             vognNummer VARCHAR(7) NOT NULL,
                             startDato DATE NOT NULL,
                             slutDato DATE,
@@ -59,77 +59,89 @@ CREATE TABLE notationer(
                            FOREIGN KEY (vognNummer) REFERENCES bil(vognNummer)
 );
 
---
 -- Medarbejdere
 INSERT INTO medArbejdere (navn, adgangskode, stilling) VALUES
-    ('demo' ,'demo' ,'Demo'    ),
-    ('demo1','demo1','Dataregistrering'    ),
-    ('demo2','demo2','Skade_&_Udbedring'   ),
-    ('demo3','demo3','Forretningsudviklere');
+('demo', 'demo', 'Demo'),
+('demo1', 'demo1', 'Dataregistrering'),
+('demo2', 'demo2', 'Skade_&_Udbedring'),
+('demo3', 'demo3', 'Forretningsudviklere');
 
--- Biltyper (3 originale + 4 nye)
+-- Biltyper
 INSERT INTO bilType (mærke, model, udstyrsniveau, stålPris, afgift, udledning_Co2) VALUES
-    ('Toyota',     'Corolla', 'Basic',       200000, 40000, 95 ),
-    ('Honda',      'Civic',   'Deluxe',      220000, 42000, 90 ),
-    ('Ford',       'Focus',   'Premium',     210000, 41000, 92 ),
-    ('BMW',        '320i',    'Sport',       350000, 70000, 120),
-    ('Audi',       'A3',      'Business',    330000, 66000, 110),
-    ('Volkswagen', 'Golf',    'Comfortline', 280000, 56000, 100),
-    ('Hyundai',    'Ioniq',   'Electric',    300000, 11,     1  );
+('Toyota', 'Corolla', 'Basic', 200000, 40000, 95),
+('Honda', 'Civic', 'Deluxe', 220000, 42000, 90),
+('Ford', 'Focus', 'Premium', 210000, 41000, 92),
+('BMW', '320i', 'Sport', 350000, 70000, 120),
+('Audi', 'A3', 'Business', 330000, 66000, 110),
+('Volkswagen', 'Golf', 'Comfortline', 280000, 56000, 100),
+('Hyundai', 'Ioniq', 'Electric', 300000, 11, 1);
 
--- Lagerlokationer (6 stk.)
+-- Lagerlokationer
 INSERT INTO lager (navn, adresse) VALUES
-    ('Lager A', '123 Main St'   ),
-    ('Lager B', '456 Side St'   ),
-    ('Lager C', '789 Back St'   ),
-    ('Lager D', '135 North Ave' ),
-    ('Lager E', '246 South Blvd'),
-    ('Lager F', '369 East Lane' );
+('Lager A', '123 Main St'),
+('Lager B', '456 Side St'),
+('Lager C', '789 Back St'),
+('Lager D', '135 North Ave'),
+('Lager E', '246 South Blvd'),
+('Lager F', '369 East Lane');
 
--- Biler (både eksisterende og nye)
-INSERT INTO bil (vognNummer, stelNummer, bilType_Id, lager_Id, status) VALUES
-    ('AB12345', 'WAUZZZ8V4DN674512', 1, 1, 'Tilgængelig'    ),
-    ('CD67890', '1HGBH41JXMN109169', 2, 2, 'Udlejet'        ),
-    ('EF56789', '1FMCU0C72CK451093', 3, 3, 'Vedligeholdelse'),
-    ('XY92831', '1HGCM82633A004321', 4, 4, 'Tilgængelig'    ),
-    ('QT63829', '2FTRX18W1XCA98765', 5, 4, 'Slettet'        ),
-    ('BH10475', 'WDBUF70J94A345678', 6, 5, 'Udlejet'        ),
-    ('KU73190', '3N1AB7AP7HY256789', 7, 5, 'Vedligeholdelse'),
-    ('ZA87364', 'JN1CV6EK9BM234567', 4, 6, 'Slettet'        ),
-    ('MW28374', '1N4AL3AP9FC198765', 5, 6, 'Udlejet'        ),
-    ('LD98430', 'WA1DGAFE6FD017654', 6, 1, 'Tilgængelig'    ),
-    ('RP19383', 'SALWR2VF2FA623890', 7, 2, 'Tilgængelig'    ),
-    ('TE48923', 'KM8J33A40GU265431', 4, 3, 'Slettet'        ),
-    ('CG57492', '5UXWX9C5XG0D34567', 5, 4, 'Tilgængelig'    );
+-- Biler (inkl. kørteKm for simulere vore logik for solgte biler)
+INSERT INTO bil (vognNummer, stelNummer, bilType_Id, lager_Id, status, kørteKm) VALUES
+('AB12345', 'WAUZZZ8V4DN674512', 1, 1, 'tilgængelig', 0),
+('CD67890', '1HGBH41JXMN109169', 2, 2, 'tilgængelig', 0),
+('EF56789', '1FMCU0C72CK451093', 3, 3, 'tilgængelig', 0),
+('XY92831', '1HGCM82633A004321', 4, 4, 'tilgængelig', 0),
+('QT63829', '2FTRX18W1XCA98765', 5, 4, 'tilgængelig', 0),
+('BH10475', 'WDBUF70J94A345678', 6, 5, 'udlejet', 0),
+('KU73190', '3N1AB7AP7HY256789', 7, 5, 'udlejet', 0),
+('ZA87364', 'JN1CV6EK9BM234567', 4, 6, 'udlejet', 0),
+('MW28374', '1N4AL3AP9FC198765', 5, 6, 'udlejet', 0),
+('LD98430', 'WA1DGAFE6FD017654', 6, 1, 'solgt', 120000),
+('RP19383', 'SALWR2VF2FA623890', 7, 2, 'solgt', 98000),
+('TE48923', 'KM8J33A40GU265431', 4, 3, 'solgt', 110500),
+('CG57492', '5UXWX9C5XG0D34567', 5, 4, 'solgt', 102000);
 
+-- Lejeaftaler (kun for biler med status 'udlejet' eller 'solgt')
 
--- Lejeaftaler
+-- UNLIMITED aftaler (udlejet biler)
 INSERT INTO lejeAftaler (kunde_Navn, vognNummer, startDato, slutDato, detaljer) VALUES
-    ('Joe', 'CD67890', '2025-01-01', '2025-01-15', 'JoeTest'),
-    ('Joe', 'AB12345', '2025-02-01', '2025-02-28', 'JoeTest'),
-    ('Joe', 'EF56789', '2025-03-01', '2025-03-10', 'JoeTest'),
-    ('Joe', 'BH10475', '2025-04-05', '2025-04-20', 'JoeTest'),
-    ('Joe', 'LD98430', '2025-05-01', '2025-06-14', 'JoeTest'),
-    ('Joe', 'RP19383', '2025-06-10', '2025-06-25', 'JoeTest'),
-    ('Joe', 'CG57492', '2025-07-01', '2025-07-10', 'JoeTest'),
-    ('Joe', 'XY92831', '2025-08-01', '2025-08-15', 'JoeTest');
+('Anders Hansen', 'BH10475', '2025-02-01', NULL, 'Unlimited aftale - minimum 3 måneder'),
+('Mette Jensen', 'KU73190', '2025-01-15', NULL, 'Unlimited aftale - minimum 3 måneder'),
+('Peter Nielsen', 'ZA87364', '2025-03-01', NULL, 'Unlimited aftale - minimum 3 måneder');
 
--- Notationer uden aftale (aftale_Id IS NULL)
-INSERT INTO notationer (aftale_Id, vognNummer, beskrivelse, pris) VALUES
-    (NULL, 'XY92831', 'Ridse på venstre dør',      1500),
-    (NULL, 'BH10475', 'Service udført',            11   ),
-    (NULL, 'KU73190', 'Skift af dæk',              3500),
-    (NULL, 'TE48923', 'Lakskade udbedret',         2200),
-    (NULL, 'LD98430', 'Bil rengjort og klargjort', 11   );
+-- LIMITED aftaler (udlejet biler)
+INSERT INTO lejeAftaler (kunde_Navn, vognNummer, startDato, slutDato, detaljer) VALUES
+('Lone Sørensen', 'MW28374', '2025-01-01', '2025-06-01', 'Limited 5 måneder');
 
--- Notationer med aftale (aftale_Id IS NOT NULL)
+-- SOLGTE aftaler (status 'solgt')
+INSERT INTO lejeAftaler (kunde_Navn, vognNummer, startDato, slutDato, detaljer) VALUES
+('Klaus Mortensen', 'LD98430', '2024-01-01', '2024-09-01', 'Solgt bil, limited lejeaftale over 8 måneder'),
+('Anette Pedersen', 'RP19383', '2024-02-01', '2024-10-01', 'Solgt bil, limited lejeaftale over 8 måneder'),
+('Thomas Olsen', 'TE48923', '2024-03-01', '2024-11-01', 'Solgt bil, limited lejeaftale over 8 måneder'),
+('Sofie Hansen', 'CG57492', '2024-04-01', '2024-12-01', 'Solgt bil, limited lejeaftale over 8 måneder');
+
+-- Notationer - kun bil (NULL i aftale_Id)
 INSERT INTO notationer (aftale_Id, vognNummer, beskrivelse, pris) VALUES
-    (1, 'CD67890', 'Extra insurance',      500 ),
-    (1, 'CD67890', 'Additional driver',    300 ),
-    (2, 'AB12345', 'GPS device',           100 ),
-    (3, 'EF56789', 'Child seat',           75  ),
-    (1, 'CD67890', 'Ridse ved aflevering', 1200),
-    (1, 'CD67890', 'Manglende brændstof',  400 ),
-    (2, 'AB12345', 'Ekstra rengøring',     300 ),
-    (3, 'EF56789', 'Sen aflevering',       500 ),
-    (3, 'EF56789', 'Skade på fælg',        800 );
+(NULL, 'AB12345', 'Serviceeftersyn udført', 2500),
+(NULL, 'CD67890', 'Ny vinterdæk monteret', 3200),
+(NULL, 'EF56789', 'Lakskade på bagskærm', 4500);
+
+-- Notationer - tilknyttet lejeaftaler (aftale_Id + vognNummer)
+-- Antager aftale_Id i rækkefølge, som de blev indsat ovenfor:
+
+-- Udlejet biler lejeaftaler: aftale_Id 6-9
+-- Solgte biler lejeaftaler: aftale_Id 10-13
+
+INSERT INTO notationer (aftale_Id, vognNummer, beskrivelse, pris) VALUES
+
+-- Udlejet biler notationer:
+(1, 'BH10475', 'Rengøring efter lejeperiode', 500),
+(2, 'KU73190', 'Ekstra kilometer afregnet', 1200),
+(3, 'ZA87364', 'Forsinkelse i aflevering', 800),
+(4, 'MW28374', 'Småskader repareret', 2200),
+
+-- Solgte biler notationer:
+(5, 'LD98430', 'Stenslag på forrude', 1500),
+(6, 'RP19383', 'Ridser i højre dør', 2000),
+(7, 'TE48923', 'Bule på bagklap', 3500),
+(8, 'CG57492', 'Mindre lakskade på kofanger', 1800);
