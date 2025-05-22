9687ba7c71b1ce94e115df3bd966b909fb0b468b
Flamingotough <simongotfred@hotmail.com>
2025-05-22 12:01:13 +0200

    Opdatering til Bruger

--- src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/model/Bruger.java
+++ src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/model/Bruger.java
@@ -1,6 +1,6 @@
 package com.g6.data24d_s2_g6_eksamensprojekt.model;
 
-public class Bruger extends Entitet
+public class Bruger extends Person
 {
     public enum Stilling
     {
@@ -16,7 +16,7 @@ public class Bruger extends Entitet
     public Bruger(int medArbejder_Id, String navn, String adgangskode, String stilling) {
         super(medArbejder_Id, navn);
         this.adgangskode = adgangskode;
-        this.stilling =  Stilling.valueOf(stilling);
+        this.stilling =  Stilling.valueOf(stilling.toUpperCase());
     }
 
     public int getMedArbejder_Id() {
@@ -26,14 +26,24 @@ public class Bruger extends Entitet
     public String getAdgangskode() {
         return adgangskode;
     }
-    public void setAdgangskode(String adgangskode) {
+    public void   setAdgangskode(String adgangskode) {
         this.adgangskode = adgangskode;
     }
 
     public String getStilling() {
         return stilling.name();
     }
-    public void setStilling(String stilling) {
-        this.stilling =  Stilling.valueOf(stilling);
+    public void   setStilling(String stilling) {
+        this.stilling =  Stilling.valueOf(stilling.toUpperCase());
+    }
+
+    public boolean erStilling(String... tilladteStillinger)
+    {
+        if (this.stilling == Stilling.DEMO) return true;
+        for (String stilling : tilladteStillinger)
+        {
+            if (this.stilling.name().equalsIgnoreCase(stilling)) return true;
+        }
+        return false;
     }
 }
