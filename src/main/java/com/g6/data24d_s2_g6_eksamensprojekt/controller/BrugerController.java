be6373aab120daa5a1d6525c0cac25f46ad16f95
alex <albo0003@kea.stud.dk>
2025-05-25 11:20:56 +0200

    BrugerController kommentare

--- src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/controller/BrugerController.java
+++ src/main/java/com/g6/data24d_s2_g6_eksamensprojekt/controller/BrugerController.java
@@ -20,12 +20,16 @@ public class BrugerController {
     @Autowired
     BrugerRepository brugerRepository;
 
+    // getmappingen for "/", bruges til at omdirigere, hvis de ikke er logget ind omdirigerer den til logind siden,
+    // ellers så til visbiler siden som bruges som forside på vores hjemmeside
     @GetMapping("/")
     public String hjemmeSide(HttpServletRequest request, Model model) {
         HttpSession session = faaSession(request, model, "data", "skade", "forretning");
         if(session == null) return "redirect:/Logind";
-        return "index";
+        return "redirect:/VisBiler";
     }
+    // fra alle andre getmappings, hvis der ikke er nogen session bliver de omdirigeret til denne getmapping ("/Logind")
+    // returnerer logind html siden
     @GetMapping("/Logind")
     public String logInd(HttpServletRequest request, Model model) {
         HttpSession session = request.getSession(false);
@@ -37,7 +41,10 @@ public class BrugerController {
 
         return "logind";
     }
-
+    // bliver sendt her til fra logind html siden, den tager imod 2 parametre, navn og adgangskode.
+    // den kalder metoden hentbruger som henter brugeren i databasen som har det navn og adgangskode.
+    // hvis brugeren eksistere og derved er i databasen, så laver den en session for at indikere at man er logget ind.
+    // ellers omdirigere den tilbage til logind siden så man kan prøve at logge ind igen.
     @GetMapping("/OmdirigerLogind")
     public String omdirigerLogInd(HttpServletRequest request, Model model) {
         String navn = request.getParameter("navn");
@@ -52,13 +59,17 @@ public class BrugerController {
         }
         return "redirect:/Logind";
     }
-
+    // man bliver sendt her til fra knappen i headeren, som hedder log Ud.
+    // stopper sessionen, hvilket er måden vi har valgt at indikere om man er logget ind eller ej.
+    // derefter omdirigere den en til logind siden.
     @GetMapping("/LogUd")
     public String logUd(HttpServletRequest request, Model model) {
         request.getSession().invalidate();
         return "redirect:/Logind";
     }
-
+    // man bliver sendt her til fra knappen i headeren, som hedder Register Ny Bruger.
+    // den får bare en masse variabler fra sessionen som bliver givet af omdirigerRegistrer metoden før den omdirigere hertil
+    // den sætter dem ind i modelen og fjerner dem fra session igen og sender en hen til registrer html siden.
     @GetMapping("/Registrer")
     public String registrer(HttpServletRequest request, Model model) {
         HttpSession session = faaSession(request, model, "forretning");
@@ -80,11 +91,6 @@ public class BrugerController {
             model.addAttribute("adgangskode2",session.getAttribute("adgangskode2"));
             model.addAttribute("stilling", session.getAttribute("stilling"));
         }
-        // model.addAttribute("stilling", session.getAttribute("stilling"));
-
-       /* String stilling = (String) session.getAttribute("stilling");
-        if(stilling == null)
-            stilling = "Dataregistrering";*/
 
 
 
@@ -100,7 +106,11 @@ public class BrugerController {
 
         return "registrer";
     }
-
+    // man bliver sendt hertil fra registrer html siden
+    // den får en masse parametre som den sætter i sessionen som bruges i "/Registrer" getmappingen
+    // hvis den ikke får et medarbejder id så skal den lave en ny bruger.
+    // den tjekker om adgangskoden og verificer adgangskode er det samme og om der allerede eksistere en bruger med det samme navn. ellers opretter den en ny medarbejder
+    // hvis den får et medarbejderId så skal den ikke registrere en ny medarbejder, men istedet opdater den medarbejder hvis medarbejderId det er.
     @GetMapping("/OmdirigerRegistrer")
     public String omdirigerRegistrer(HttpServletRequest request, Model model) {
         HttpSession session = faaSession(request, model, "forretning");
@@ -119,9 +129,7 @@ public class BrugerController {
         session.setAttribute("adgangskode2", adgangskode2);
         session.setAttribute("medarbejderNavn", medarbejderNavn);
 
-        //if(stilling.toCharArray()[stilling.length() - 1] == '_'){
-        //stilling = stilling.replace("_", "");
-        if(navn!= null && adgangskode!= null && adgangskode2 != null && stilling!= null){
+        if(medarbejderId == null){
 
             if(!adgangskode.equals(adgangskode2)){
                 session.setAttribute("registrerBesked", "Verificer Adgangskode er ikke det samme som Adgangskode");
@@ -134,40 +142,25 @@ public class BrugerController {
                 brugerRepository.gemBruger(navn, adgangskode, stilling);
             }
         }
-        if(medarbejderId != null){
+       else {
             Bruger bruger = brugerRepository.hentBruger(navn);
 
             if(bruger != null && bruger.getMedArbejder_Id() != Integer.parseInt(medarbejderId)){
                 session.setAttribute("registrerBesked", "Bruger med samme navn eksisterer allerede");
-                return "redirect:/Registrer?medarbejderId=" + medarbejderId;
+
             }
             else{
                 session.setAttribute("registrerBesked", "Bruger opdateret");
                 brugerRepository.opdaterBruger(navn, adgangskode, stilling, Integer.parseInt(medarbejderId));
             }
 
-
-        }
-           /* else {
-                if(navn.isEmpty()){
-                    session.setAttribute("manglendeInput", 0);
-                }
-                else if(adgangskode.isEmpty()){
-                    session.setAttribute("manglendeInput", 1);
-                }
-                else if(adgangskode2.isEmpty()){
-                    session.setAttribute("manglendeInput", 2);
-                }
-            }*/
-       /* }
-        else {
-
-        }*/
-
+            return "redirect:/Registrer?medarbejderId=" + medarbejderId;
+       }
 
         return "redirect:/Registrer";
     }
-
+    // man bliver sendt her til fra knappen på registrer html siden, som hedder Rediger Eksisterende Bruger.
+    // den giver en liste med alle medarbejdere og sender en hen til soegBruger html siden.
     @GetMapping("/SoegBruger")
     public String soegBruger(HttpServletRequest request, Model model) {
         HttpSession session = faaSession(request, model, "forretning");
@@ -180,7 +173,8 @@ public class BrugerController {
 
         return "soegBruger";
     }
-
+    // man bliver sendt her til fra soegBruger html siden.
+    // den får parameteret medarbejderNavn som er den der skal bruges til at søges efter og omdirigere til "/SoegBruger"
     @GetMapping("/OmdirigerSoegBruger")
     public String omdirigerSoegBruger(HttpServletRequest request, Model model) {
         HttpSession session = faaSession(request, model, "forretning");
@@ -191,22 +185,6 @@ public class BrugerController {
         return "redirect:/SoegBruger";
     }
 
-    /*@GetMapping("/RedigerBruger")
-    public String redigerBruger(HttpServletRequest request, Model model) {
-        HttpSession session = faaSession(request, model, new String[]{"forretnings"});
-        if(session == null) return "redirect:/Logind";
-
-        return "index";
-    }*/
-
-    @GetMapping("/OmdirigerRedigerBruger")
-    public String omdirigerRedigerBruger(HttpServletRequest request, Model model) {
-        HttpSession session = faaSession(request, model, "forretning");
-        if(session == null) return "redirect:/Logind";
-
-        return "redirect:/Registrer";
-    }
-
     @PostMapping("/saetValuta")
     @ResponseStatus(HttpStatus.RESET_CONTENT)
     public void saetValuta(HttpServletRequest request, Model model)
@@ -215,14 +193,16 @@ public class BrugerController {
         assert session != null;
         session.setAttribute("valuta", request.getParameter("valutaCheck"));
     }
-
+    // metoden kaldes efter man er logget ind, den laver en ny session og giver den en bruger,
+    // som er den bruger der er logget ind
     private void saetSession(HttpServletRequest request, Bruger bruger, Model model) {
         HttpSession session = request.getSession();
         session.setMaxInactiveInterval(1200);
         session.setAttribute("aktivBruger", bruger);
         model.addAttribute("aktivBruger", bruger);
     }
-
+    // metoden kaldes i starten af alle getmappings,
+    // den tager imod sessionen og tilføjer valuta og bruger til modelen
     static public HttpSession faaSession(HttpServletRequest request, Model model, String... tilladteStillinger){
         HttpSession session = request.getSession(false);
         if(session != null){
